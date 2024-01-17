e66c86b827c3e11d2ea1e0abc8ff95653f3a7a2a
liyu-lakew <lakewliya@gmail.com>
2024-01-17 01:33:14 -0500

    first commit

---
+++ db/dbConfig.js
@@ -0,0 +1,20 @@
+// const mysql2 = require('mysql2')
+// const dbConnection = mysql2.createPool({
+//     user:process.env.USER,
+//     database:process.env.DATABASE,
+//     host:"localhost",
+//     password:process.env.PASSWORD,
+//     connectionLimit:10
+    
+
+// })
+// module.exports=dbConnection.promise()
+const mysql2 = require('mysql2')
+const dbConnection = mysql2.createPool({
+    user:process.env.USER,
+    database:process.env.DATABASE,
+    host:process.env.HOST,
+    password:process.env.PASSWORD,
+    connectionLimit:10
+})
+module.exports=dbConnection.promise()
